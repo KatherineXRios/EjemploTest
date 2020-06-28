c2a6be09d6f1c7f697cc3f6c5e60e9bf46700fd5
Katherine Rios <katherinex.rios.lopez@gmail.com>
2020-06-28 17:17:53 -0400

    ModificacionIndex

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-//INDEX NECESARIO
+//Ejemplo de Async Await
+const dbSeries =
+[
+    {
+        id : 1,
+        name: 'Naruto',
+        idPersona: 1
+    },
+    {
+        id : 2,
+        name: 'Death Note',
+        idPersona: 2
+    },
+    {
+        id : 3,
+        name: 'Attack on Titan',
+        idPersona: 3
+    }
+];
+const dbPersonajes =
+[
+    {
+        id: 1,
+        nombre: 'Naruto Uzumaki'
+    },
+    {
+        id: 2,
+        nombre: 'Light Yamagi'
+    },
+    {
+        id: 3,
+        nombre: 'Eren Jagger'
+    }
+]
+
+async function getAnime(id){
+        const anime = dbAnime.find(anime => anime.id === id);
+        if(!anime){
+            const error = new Error();
+            error.message = 'El anime no existe en la DB';
+            throw error;
+            }
+            return anime;
+}
+async function getPersonaje(id){
+
+        const personaje = dbPersonajes.find(personaje => personaje.id === id);
+        if(!personaje){
+            const error = new Error();
+            error.message = 'El personaje no existe en la DB';
+            throw error;
+                 }
+                 return personaje;
+
+}
+
+async function main(){
+    try{
+        const anime = await getAnime(3);
+        const personaje = await getPersonaje(15);
+        console.log(`Este anime ${anime.name} es interpretado por ${personaje.nombre}`);
+    }catch(e){
+    console.log(e.message)
+    }    
+}
+
+main();
